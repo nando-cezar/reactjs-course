89ebc131674abe6ed1be849997728cff4f100819
nando-cezar <luisfernando_cezar@hotmail.com>
2020-12-20 18:50:46 -0300

    react-init | init styled-components;

--- react-init/src/App.js
+++ react-init/src/App.js
@@ -1,6 +1,21 @@
 import React from 'react';
+import './App.css';
+import styled from 'styled-components';
 
-function formatarNome(usuario){
+
+const Site = styled.div`
+  width: 400px;
+  height: 400px;
+  background-color: #00A100;
+`;
+
+const Botao = styled.button`
+  font-size: 19px;
+  padding: 10ox 15px;
+  //background-color: ${props => props.color || "#00FF00"};
+  background-color: ${props => props.ativo === true ? "#CCC" : "#00FF00"};
+`;
+/* function formatarNome(usuario){
   return usuario.nome + ' ' + usuario.sobrenome;
 }
 
@@ -8,22 +23,47 @@ function HelloWorld(props){
   return <h1>Olá mundo, {props.name} </h1>
 }
 
+function Avatar(props){
+  return(
+    <div className="avatar">
+      <img src={props.user.url} alt={props.user.name}/>
+      <br/>
+      <span>{props.user.name}</span>
+    </div>
+  );
+} */
+
 const App = () => {
 
-  let imagem = 'https://www.google.com.br/google.jpg';
+  /* let imagem = 'https://www.google.com.br/google.jpg';
 
   let usuario = {
     nome: 'Luís Fernando',
     sobrenome: 'Cezar dos Santos'
   };
 
-  return (
-    <>
-      <div>Meu nome é {formatarNome(usuario)}</div>
-      <img src={imagem} />
+  let user = {
+    url: 'https://www.google.com.br/google.jpg',
+    name: 'Luís Fernando'
+  }; */
 
-      <HelloWorld name="Luís Fernando"/>
-    </>
+
+  /*
+    <div>Meu nome é {formatarNome(usuario)}</div>
+    <img src={imagem} />
+
+    <HelloWorld name={usuario.nome}/>
+
+    <Avatar user={user}/>
+  */ 
+  return (
+    <Site>
+      <Botao color="#FF0000">Clique aqui!</Botao>
+      <Botao color="#0000FF">Clique aqui!</Botao>
+      <Botao>Clique aqui!</Botao>
+      <Botao ativo={true}>Clique aqui!</Botao>
+      <Botao ativo={false}>Clique aqui!</Botao>
+    </Site>
   );
 }
 
